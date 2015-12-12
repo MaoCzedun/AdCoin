482492278a6b280fdf18d83f44aeb4bb682fc192
pahanium <pportyanko@gmail.com>
2015-12-12 18:22:19 +0200

    change db connection

--- app/config/db.php
+++ app/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=hackathon',
-    'username' => 'hackathon',
-    'password' => 'hackathon',
+    'dsn' => 'mysql:host=al23dd.mysql.ukraine.com.ua;dbname=al23dd_db',
+    'username' => 'al23dd_db',
+    'password' => 'JtMYGPF4',
     'charset' => 'utf8',
     'tablePrefix' => '',
     'enableSchemaCache' => true,
